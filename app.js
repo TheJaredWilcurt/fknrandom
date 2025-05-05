e0b954d18c17f476ea27e4ac205cbc59fd5b505f
The Jared Wilcurt <4629794+TheJaredWilcurt@users.noreply.github.com>
2025-05-05 01:01:51 -0400

    randomizer working

--- app.js
+++ app.js
@@ -61,6 +61,8 @@ const imageMap = Object.freeze([
   }
 ]);
 
+const APP_NAME = 'fknrandom'
+
 Vue.createApp({
   data: function () {
     return {
@@ -69,16 +71,46 @@ Vue.createApp({
       topLeft: null,
       topRight: null,
       bottomLeft: null,
-      bottomRight: null
+      bottomRight: null,
+      character: 'doc',
+      skin: 1,
+      volume: 100,
+      characters: {
+        'doc': 5,
+        'mario': 5,
+        'luigi': 4,
+        'bowser': 4,
+        'peach': 5,
+        'yoshi': 6,
+        'dk': 5,
+        'falcon': 6,
+        'gannon': 5,
+        'falco': 4,
+        'fox': 4,
+        'ness': 4,
+        'icies': 4,
+        'kirby': 6,
+        'samus': 5,
+        'zelda': 5,
+        'link': 5,
+        'young-link': 5,
+        'pichu': 4,
+        'pikachu': 4,
+        'puff': 5,
+        'mewtwo': 4,
+        'game-and-watch': 4,
+        'marth': 5,
+        'roy': 5
+      },
+      personSoundMap: {
+        bobby: 71,
+        hank: 325
+      }
     };
   },
   methods: {
     getRandomSound: function (person) {
-      const personMaxMap = {
-        bobby: 71,
-        hank: 325
-      };
-      const max = personMaxMap[person];
+      const max = this.personSoundMap[person];
       const random = Math.ceil(Math.random() * max);
       return '_sound/' + person + '/' + random + '.mp3';
     },
@@ -87,6 +119,19 @@ Vue.createApp({
     },
     getRandomHank: function () {
       return this.getRandomSound('hank');
+    },
+    playRandomSound: function () {
+      const bobby = this.personSoundMap.bobby;
+      const hank = this.personSoundMap.hank;
+      let file;
+      if (Math.round(Math.random() * (bobby + hank)) < bobby) {
+        file = this.getRandomBobby();
+      } else {
+        file = this.getRandomHank();
+      }
+      let sound = new Audio(file);
+      sound.volume = this.volume / 100;
+      sound.play();
     },
     getRandomCorner: function () {
       const imagesAmount = imageMap.length;
@@ -100,15 +145,61 @@ Vue.createApp({
       }
       return corners;
     },
+    getRandomCharacter: function () {
+      const characterNames = Object.keys(this.characters);
+      const charactersAmount = characterNames.length;
+      this.character = characterNames[Math.floor(Math.random() * charactersAmount)];
+      const skinsAmount = this.characters[this.character];
+      this.skin = Math.ceil(Math.random() * skinsAmount);
+    },
+    rollForCharacter: function () {
+      this.playRandomSound();
+      const length = 20;
+      for (let i = 0; i < length; i++) {
+        setTimeout(() => {
+          this.getRandomCharacter();
+        }, 20 * i);
+      }
+    },
     initializeCorners: function () {
       const indices = this.getRandomCorners();
       this.topLeft = imageMap[indices[0]];
       this.topRight = imageMap[indices[1]];
       this.bottomLeft = imageMap[indices[2]];
       this.bottomRight = imageMap[indices[3]];
+    },
+    loadSettings: function () {
+      const settings = JSON.parse(localStorage.getItem(APP_NAME));
+      if (settings) {
+        this.animatedBg = settings.animatedBg;
+        this.showImages = settings.showImages;
+        this.volume = settings.volume;
+      }
+    },
+    saveSettings: function () {
+      localStorage.setItem(APP_NAME, this.dataToSave);
+    }
+  },
+  computed: {
+    characterIndex: function () {
+      return Object.keys(this.characters).indexOf(this.character);
+    },
+    dataToSave: function () {
+      return JSON.stringify({
+        animatedBg: this.animatedBg,
+        showImages: this.showImages,
+        volume: this.volume
+      });
+    }
+  },
+  watch: {
+    dataToSave: function (argument) {
+      this.saveSettings();
     }
   },
   created: function () {
+    this.loadSettings();
     this.initializeCorners();
+    this.rollForCharacter();
   }
 }).mount('#app');
