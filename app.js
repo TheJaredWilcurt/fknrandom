654cbd8c4fcfbb13f96d6de435b6bac359fd444a
TheJaredWilcurt <TheJaredWilcurt@users.noreply.github.com>
2025-05-07 19:39:48 -0400

    Green screen

--- app.js
+++ app.js
@@ -155,14 +155,80 @@ const MenuItem = {
   }
 };
 
-Vue.createApp({
-  components: {
-    CornerImage,
-    MenuItem
+
+const MenuItems = {
+  name: 'MenuItems',
+  template: `
+    <div
+      class="menu-item menu-items"
+      :class="{
+        active: hover,
+        first: modelValue === options[0],
+        last: modelValue === options[options.length - 1]
+      }"
+      role="button"
+      tabindex="0"
+      @click="toggle"
+      @keyup.enter="toggle"
+      @keydown.space.prevent="toggle"
+      @mouseover="hover = true"
+      @mouseout="hover = false"
+      @focus="hover = true"
+      @blur="hover = false"
+    >
+      <div class="menu-item-bg"></div>
+      <div class="menu-item-border"></div>
+      <div class="menu-item-settings-bg"></div>
+      <div class="menu-item-half-circle"></div>
+      <div class="menu-item-full-circle"></div>
+      <div class="menu-item-label"><slot></slot></div>
+      <div class="menu-item-switch-curtains menu-items-switch-curtains">
+        <span
+          v-for="(option, index) in options"
+          :class="'menu-item-' + index"
+          :style="'left: ' + (options.indexOf(modelValue) * -100) + 'px'"
+        >
+          {{ option.toUpperCase() }}
+        </span>
+      </div>
+      <div class="menu-item-previous"></div>
+      <div class="menu-item-next"></div>
+    </div>
+  `,
+  props: {
+    options: Array,
+    modelValue: String,
+    volume: Number
   },
   data: function () {
     return {
-      animatedBg: true,
+      hover: false
+    };
+  },
+  methods: {
+    toggle: function () {
+      const sound = new Audio('_sound/click.wav');
+      sound.volume = this.volume / 100;
+      sound.play();
+      const currentValueIndex = this.options.indexOf(this.modelValue);
+      let nextValue = this.options[currentValueIndex + 1];
+      if (!nextValue) {
+        nextValue = this.options[0];
+      }
+      this.$emit('update:modelValue', nextValue);
+    }
+  }
+};
+
+Vue.createApp({
+  components: {
+    CornerImage,
+    MenuItem,
+    MenuItems
+  },
+  data: function () {
+    return {
+      background: 'animated',
       showImages: true,
       trueRandom: false,
       topLeft: null,
@@ -349,7 +415,7 @@ Vue.createApp({
     loadSettings: function () {
       const settings = JSON.parse(localStorage.getItem(APP_NAME));
       if (settings) {
-        this.animatedBg = settings.animatedBg;
+        this.background = settings.background;
         this.showImages = settings.showImages;
         this.trueRandom = settings.trueRandom;
         this.volume = settings.volume;
@@ -365,7 +431,7 @@ Vue.createApp({
     },
     dataToSave: function () {
       return JSON.stringify({
-        animatedBg: this.animatedBg,
+        background: this.background,
         showImages: this.showImages,
         trueRandom: this.trueRandom,
         volume: this.volume
